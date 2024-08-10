feece3135a3a2bc912a380bbe42455a8e39e58d9
GavinSun0921 <gavinsun0921@foxmail.com>
2024-08-10 18:00:20 +0800

    rename header name

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #include <cstdio>
 
-#include "include/simple_cpp20_thread_pool.hpp"
+#include "include/thread_pool_lite.hpp"
 
 double worker(int k) {
-    double res = 4.0 / (2 * k + 1.0);
-    return k % 2 == 0 ? res : -1.0 * res;
+  double res = 4.0 / (2 * k + 1.0);
+  return k % 2 == 0 ? res : -1.0 * res;
 }
 
 double pi(int n) {
-    ThreadPoolExecutor pool;
-    std::vector<std::future<double>> futures;
-    futures.reserve(n);
-    for (int i = 0; i < n; ++i) {
-        futures.push_back(pool.submit(worker, i));
-    }
-    double p = 0.0;
-    for (auto &future: futures) {
-        p += future.get();
-    }
-    return p;
+  ThreadPoolExecutor pool;
+  std::vector<std::future<double>> futures;
+  futures.reserve(n);
+  for (int i = 0; i < n; ++i) {
+    futures.push_back(pool.submit(worker, i));
+  }
+  double p = 0.0;
+  for (auto &future : futures) {
+    p += future.get();
+  }
+  return p;
 }
 
 int main() {
-    printf("Pi = %.12f\n", pi(1'000'000));
+  printf("Pi = %.12f\n", pi(1'000'000));
+  return 0;
 }
